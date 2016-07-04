c94c3fe6c207ec504fb53ba4c730de1bd92b357b
krturner <krisleoturner@gmail.com>
2016-07-04 11:23:44 -0600

    Updated ruby

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render text: "MovieLibrary v0.1"
+    render text: "MovieLibrary v0.2"
   end
 end
